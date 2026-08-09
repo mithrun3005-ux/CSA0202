c79d9c8f53cb1eba96ea15b9def2643a1c908360
mithrun3005-ux <mithrun3005@gmail.com>
2026-08-09 09:20:51 +0530

    Add uppercase or lowercase C program

---
+++ C-Programming/02_Decision_Making/uppercase_or_lowercase.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+
+    if (ch >= 'A' && ch <= 'Z')
+        printf("Uppercase");
+    else if (ch >= 'a' && ch <= 'z')
+        printf("Lowercase");
+    else
+        printf("Not an alphabet character");
+
+    return 0;
+}
+
+/*
+Sample Input:
+G
+
+Sample Output:
+Uppercase
+*/
