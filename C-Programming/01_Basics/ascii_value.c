4e5628d1fe821251bfb2dd872232e42b5bc1b067
mithrun3005-ux <mithrun3005@gmail.com>
2026-08-09 09:20:31 +0530

    Add ASCII value C program

---
+++ C-Programming/01_Basics/ascii_value.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    printf("ASCII value of %c = %d", ch, ch);
+    return 0;
+}
+
+/*
+Sample Input:
+A
+
+Sample Output:
+ASCII value of A = 65
+*/
